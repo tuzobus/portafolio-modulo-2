8e49e4eb66259eff8fa52e9c0a77f420f6884ba4
Ivan <upp.estancia@gmail.com>
2026-09-05 19:35:55 -0600

    feat: agrega pruebas con hiperparámetros ajustados

--- framework_model.py
+++ framework_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split, StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
@@ -81,3 +81,64 @@ print(classification_report(y_val, val_pred))
 
 print("\nConfusion matrix:")
 print(confusion_matrix(y_val, val_pred))
+
+
+# Pruebas con hiperparámetros ajustados
+param_grid = {
+    "n_estimators": [100, 300, 500],
+    "max_depth": [None, 5, 10, 20],
+    "min_samples_split": [2, 5, 10],
+    "min_samples_leaf": [1, 2, 4],
+    "max_features": ["sqrt", "log2", 0.5],
+    "class_weight": [None, "balanced"],
+}
+
+model = RandomForestClassifier(random_state=67)
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=67)
+
+grid_search = GridSearchCV(
+    model, param_grid, cv=cv, scoring="accuracy", n_jobs=-1, verbose=2
+)
+grid_search.fit(X_train, y_train)
+
+best = grid_search.best_estimator_
+
+print("Best parameters:", grid_search.best_params_)
+print("Best score:", grid_search.best_score_)
+
+
+# Actualización del modelo con mejores hiperparámetros encontrados
+val_pred = best.predict(X_test)
+
+val_accuracy = accuracy_score(y_test, val_pred)
+
+print("Validation accuracy:", val_accuracy)
+
+print("\nClassification report:")
+print(classification_report(y_test, val_pred))
+
+print("\nConfusion matrix:")
+print(confusion_matrix(y_test, val_pred))
+
+# En caso de que no se desee correr la búsqueda de hiperparámetros,
+# se pueden comentar los 2 bloques anteriores y descomentar el siguiente,
+# que contiene los mejores hiperparámetros encontrados en una búsqueda
+# realizada con anterioridad.
+
+# model = RandomForestClassifier(
+#     class_weight=None,
+#     max_depth=5,
+#     max_features='sqrt',
+#     min_samples_leaf=4,
+#     min_samples_split=2,
+#     n_estimators=100,
+# )
+# model.fit(X_train, y_train)
+# val_pred = model.predict(X_test)
+# val_accuracy = accuracy_score(y_test, val_pred)
+# print("Validation accuracy:", val_accuracy)
+# print("\nClassification report:")
+# print(classification_report(y_test, val_pred))
+# print("\nConfusion matrix:")
+# print(confusion_matrix(y_test, val_pred))
